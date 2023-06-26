8b845d60a724959576d576f233c1112928253c41
Bevis <x8163don@gmail.com>
2023-06-26 10:17:45 +0800

    feat: remove copy webpack

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,5 @@
-const CopyPlugin = require("copy-webpack-plugin");
-
 module.exports = {
-  publicPath: process.env.VUE_APP_BUILD_PATH,
-  productionSourceMap: false,
-  transpileDependencies: ["vuetify"],
-  configureWebpack: config => {
-    const plugins = [];
-
-    plugins.push(getCopyPlugin());
-    config.plugins = [...config.plugins, ...plugins];
-  }
+    publicPath: process.env.VUE_APP_BUILD_PATH,
+    productionSourceMap: false,
+    transpileDependencies: ["vuetify"],
 };
-
-function getCopyPlugin() {
-  return new CopyPlugin([{ from: "CNAME", to: "" }]);
-}
